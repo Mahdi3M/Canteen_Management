987932ebc368a304222a6f2cc812f43cf1705cf3
Mahdi3M <mahdimohtasim@gmail.com>
2023-08-10 22:54:58 +0600

    Frontend moved to Django
    Additional:
    -Contents Page created (blank)
    -Order Page created (blank)
    -History Page created (blank)

---
+++ Canteen/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from Canteen import views
+
+app_name = "Canteen"
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('profile/', views.profile, name="profile"),
+    path('contact/', views.contact, name="contact"),
+
+    path('customer_order/', views.customer_order, name="customer_order"),
+    path('customer_history/', views.customer_history, name="customer_history"),
+]
---
+++ Canteen/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#<-- ======= Pages ======= -->
+
+def home(request):
+    return render(request, "Canteen/index.html")
+
+def login(request):
+    return render(request, "Canteen/login.html")
+
+def register(request):
+    return render(request, "Canteen/register.html")
+
+def profile(request):
+    return render(request, "Canteen/profile.html")
+
+def contact(request):
+    return render(request, "Canteen/contact.html")
+
+#<-- ======= Customer ======= -->
+
+def customer_order(request):
+    return render(request, "Canteen/customer_order.html")
+
+def customer_history(request):
+    return render(request, "Canteen/customer_history.html")
+
+#<-- ======= Bar NCO ======= -->
+
+
+
+#<-- ======= Admin ======= -->
